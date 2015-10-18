b20ff3cf8b2ff59289e5ed2c38318eb624598b80
thanhtanh <thanhtanh72@gmail.com>
2015-10-18 22:08:06 +0700

    Initial Commit

---
+++ T4TextFieldValidator/T4TextFieldValidator.h
@@ -0,0 +1,19 @@
+//
+//  T4TextFieldValidator.h
+//  T4TextFieldValidator
+//
+//  Created by SB 3 on 10/18/15.
+//  Copyright © 2015 T4nhpt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for T4TextFieldValidator.
+FOUNDATION_EXPORT double T4TextFieldValidatorVersionNumber;
+
+//! Project version string for T4TextFieldValidator.
+FOUNDATION_EXPORT const unsigned char T4TextFieldValidatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <T4TextFieldValidator/PublicHeader.h>
+
+
